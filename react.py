4473130e86d58aa48b585f06b2967e58445bc0c6
ugurkan_ates <ugurkanates97@gmail.com>
2024-07-10 17:04:13 +0300

    add comparison and websearch to tasks

--- react.py
+++ react.py
@@ -2,19 +2,24 @@
 # Using GPT to work with ReACT Framework
 
 from agent import Agent
-from tasks import current_date, calculator, wiki_search
+from tasks import current_date, calculator, wiki_search,search_web,number_comparison
 from tools import Tool
 
 
-wiki_search = Tool("WikipediaSearch", wiki_search)
+# wiki_search = Tool("Wikipedia Search", wiki_search)
 calculator = Tool("Calculator", calculator)
-current_date = Tool("CurrentDate", current_date)
-
+current_date = Tool("Get Current Date", current_date)
+search_web = Tool("Internet search", search_web)
+number_comparison = Tool("Number Comparison", number_comparison)
 
 agent = Agent()
 
-agent.add_tool(wiki_search)
+# agent.add_tool(wiki_search)
 agent.add_tool(calculator)
 agent.add_tool(current_date)
+agent.add_tool(search_web)
+agent.add_tool(number_comparison)
 
-agent.react("What is the double of kaan tangoze's age?")
+# agent.react("What is the double of kaan tangoze's age ?")
+# agent.react("how old is Turkish republic ? find that and return number by doubling it ")
+# agent.react("Traffic situation in New York City for today") 
--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 import datetime
 import wikipedia
+import requests
+import json
 
 # calculator
 def calculator(operation, a, b):
@@ -17,6 +19,14 @@ def calculator(operation, a, b):
             return "Division by zero"
         return a / b
 
+# number comparison
+def number_comparison(a, b):
+    if a > b:
+        return f"{a} is greater than {b}"
+    elif a < b:
+        return f"{a} is less than {b}"
+    else:
+        return f"{a} is equal to {b}"
 
 # wiki search
 def wiki_search(search_query):
@@ -27,3 +37,105 @@ def wiki_search(search_query):
 # date func
 def current_date():
     return datetime.date.today()
+
+# serper web scraper
+# Equivalent of the search web function
+def search_web(thing_to_search_on_web: str):
+    url = "https://google.serper.dev/search"
+    # auto from API key - n_results = 10
+    payload = json.dumps({
+    "q": thing_to_search_on_web,
+    "gl": "tr"
+    })
+    headers = {
+    'X-API-KEY': 'faaa409f4026dd319021517c68dae39539b751d8',
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    results = json.loads(response.text)
+    if 'organic' in results:
+        results = results['organic']
+        string = []
+        for result in results:
+            try:
+                string.append('\n'.join([
+                        f"Title: {result['title']}",
+                        f"Link: {result['link']}",
+                        f"Snippet: {result['snippet']}",
+                        "---"
+                ]))
+            except KeyError:
+                next
+
+        content = '\n'.join(string)
+        return f"\nSearch results: {content}\n"
+    else:
+        return results
+
+def openai_tools():
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "calculator",
+                "description": "Basic calculator can do add, subtract, multiply, divide between two numbers",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "operation": {
+                            "type": "string",
+                            "description": "Add, subtract, multiply, divide",
+                        },
+                        "a": {
+                            "type": "number",
+                            "description": "First number",
+                        },
+                        "b": {
+                            "type": "number",
+                            "description": "Second number",
+                        },
+                    },
+                    "required": ["operation", "a", "b"],
+                },
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "current_date",
+                "description": "Returns current date",
+            },
+            "type": "function",
+            "function": {
+                "name": "wiki_search",
+                "description": "Searches wikipedia for the given query",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "search_query": {
+                            "type": "string",
+                            "description": "Search query for wikipedia",
+                        },
+                    },
+                    "required": ["search_query"],
+                },
+            },
+            "type": "function",
+            "function": {
+                "name": "web_scraper",
+                "description": "Searches the web for the given query",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "thing_to_search_on_web": {
+                            "type": "string",
+                            "description": "Thing to search on the web",
+                        },
+                    },
+                    "required": ["thing_to_search_on_web"],
+                },
+            },
+        },
+    ]
+    return tools
